3eb90bbaa4c1b105b305e7ab950be2e42773039d
José <josemoreno07san@gmail.com>
2024-12-22 11:04:46 -0500

    Add files via upload
    El contrato fork es la asignacion 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Practices
+# PracticaProgX
+<p>Hola gente de H9</p>
+<h1>H9 ProgX</h1>
---
+++ contrato_fork.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+class Roles_de_Personajes(ABC)
+ class Superheroes:
+  @abstractmethod
+  def atacar_a_los_malos(Villanos):
+    pass 
+ class Villanos: 
+  @abstractmethod
+  def atacar_a_los_buenos(Superheroes):
+    pass
+ class NPC: 
+  @abstractmethod
+  def no_hacer_un_carajo(null):
+    pass
+
+
+        
+ 
